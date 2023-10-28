b7cc874d07a86574339b5f1b6b825b87727285b0
ricardo <ricardoby.linkinpark@gmail.com>
2023-10-27 21:14:25 -0300

    cambio de direccion 3

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: process.env.BACKEND_URL || 'https://yppi6g7c3h.execute-api.sa-east-1.amazonaws.com/Prod/api',
+    baseURL: process.env.BACKEND_URL || 'http://localhost:8000/',
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
